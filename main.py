d167567d754a91ad2cfe67e4ad998324fbd33bab
Luke Wilson <lukewilson37@rocketmail.com>
2022-07-28 12:10:00 -0500

    handle team_name path errors

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def highlights_page():
 
 @app.route('/highlights/<team_name>')
 def team_highlights_page(team_name):
-    team_info = fetch_team_info(team_name)
+    try: team_info = fetch_team_info(team_name)
+    except: return render_template('404.html')
     return render_template('highlights.html', games=json.loads(team_info)[team_name]['schedule'])
 
 @app.errorhandler(404)
